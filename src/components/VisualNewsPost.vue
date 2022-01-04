c6aff28e5b5dd712a74f534026ce24cecd9f7517
Tch1b0 <Jpour3006@gmail.com>
2022-01-04 11:41:41 +0100

    enhance design

--- src/components/VisualNewsPost.vue
+++ src/components/VisualNewsPost.vue
@@ -1,7 +1,7 @@
 <template>
     <ion-card>
         <ion-card-content>
-            <h4>{{ newsPost.title }}</h4>
+            <ion-card-title>{{ newsPost.title }}</ion-card-title>
             <p>{{ newsPost.detail }}</p>
         </ion-card-content>
     </ion-card>
@@ -9,13 +9,14 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import { IonCard, IonCardContent } from "@ionic/vue";
+import { IonCard, IonCardContent, IonCardTitle } from "@ionic/vue";
 import { NewsPost } from "dsbmobile";
 
 export default defineComponent({
     components: {
         IonCard,
         IonCardContent,
+        IonCardTitle,
     },
     props: {
         newsPost: {
